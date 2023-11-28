aa301e1ee87a0854fce638189ca635967c6ac43d
ducna <ducna@MSI.lan>
2023-11-27 22:21:59 -0500

    Duc Nam's commit message

--- McGee_FinalProject/LocationPackage/DecryptLocation.py
+++ McGee_FinalProject/LocationPackage/DecryptLocation.py
@@ -3,8 +3,8 @@
 import json
 
 def func_1():
-    file_path_1 = "EncryptedGroupHints Fall 2023 Section 001 (1).json"
-    file_path_2 = "english-2 (1).txt"
+    file_path_1 = "EncryptedGroupHints Fall 2023 Section 001.json"
+    file_path_2 = "english-2.txt"
 
     with open(file_path_1, 'r') as file:
         data_1 = json.load(file)['McGee']
@@ -20,3 +20,5 @@ def func_1():
     result_1 = ' '.join([english_vocab_list[x] for x in data_1])
 
     return result_1
+
+print(func_1())
